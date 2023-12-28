74c244dbccb812335a0f5d1f881e752a898162a5
Sharan Thakur <sharandt19@live.com>
2023-12-28 19:29:56 +0530

    Initial

---
+++ index.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", fetchData);
+
+async function fetchData() {
+    const decoder = new TextDecoder();
+
+    let url = "https://shortstories-api.onrender.com/";
+    const response = await fetch(url);
+    const data = await response.arrayBuffer();
+
+    const textData = decoder.decode(data);
+    const jsonData = JSON.parse(textData);
+
+    console.log(jsonData);
+
+    document.getElementById("response").innerHTML = "<h2>JSONData we got:\n</h2><br>" + textData + "\n<h2>After parsing\n</h2>" 
+    + jsonData["title"] + "\n" + jsonData["story"];
+}
